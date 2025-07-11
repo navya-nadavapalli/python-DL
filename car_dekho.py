df836cb8a8e3517469371bb3801a1c82108a530a
navya-nadavapalli <nadavapallinavya@gmail.com>
2025-07-11 09:41:52 +0530

    car_loan.py

--- car_dekho.py
+++ car_dekho.py
@@ -1,6 +1,6 @@
  #defining variables
 fuel_type = "diesel" #string type of data 
-engine_displacement =1493 cc #int type of data
+engine_displacement =1493 #int type of data
 number_of_cylinders = 4 #int type of data
 seating_capacity = 5 #int type of data
 transmission_type ="Automatic" #string type of data
@@ -8,6 +8,3 @@ power_steering=True  #boolean type of dat
 power_windows_front=True #boolean type of data
 Adujustable_steering=False #boolean type of data
 wheel_covers=False #boolean type of data
-
-
-print("fuel_type");
